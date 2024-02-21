b002019511d265ba908af43a4aa44a8e1c77d679
yashwanthimarni77 <140495752+yashwanthimarni77@users.noreply.github.com>
2024-02-21 13:12:27 +0530

    Update day1.py

--- DAY 1/day1.py
+++ DAY 1/day1.py
@@ -1,13 +1,6 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a=int(input("Enter a number "))
-Enter a number 10
->>> b=int(input("Enter a number "))
-Enter a number 5
->>> if a > b :
-...     print(" a is greater ")
-... else :
-...     print(" b is greater ")
-... 
-...     
- a is greater 
+a=int(input("Enter a number "))
+b=int(input("Enter a number "))
+if a > b :
+     print(" a is greater ")
+else :
+     print(" b is greater ")

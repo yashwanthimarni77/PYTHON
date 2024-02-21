1bfa1431e672fc64d280af794519975ec14f2dd9
yashwanthimarni77 <140495752+yashwanthimarni77@users.noreply.github.com>
2024-02-21 20:13:29 +0530

    Delete day3 code1.py

--- day3 code1.py
+++
@@ -1,8 +0,0 @@
-#write a python program to print the smallest vowel repeating odd number of times in the given string
-s=input()
-s1=""
-for i in s:
-    if i in "aeiouAEIOU":
-        if s.count(i)%2!=0:
-            s1+=i
-print(min(s1))
